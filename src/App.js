a9ccfb9186e0d7a1a2afe9d2bde1aff546e87840
naso576 <70210241+naso576@users.noreply.github.com>
2024-02-15 13:23:04 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import Dashboard from "./scenes/dashboard";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Sidebar1 from "./scenes/global/Sidebar";
 import Contacts from "./scenes/contacts";
 import SignInSide from "./scenes/sign-in";
 import SignUp from "./scenes/signup";
 import PatientProfile from "./scenes/profiles";
 import ManageProfile from "./scenes/manageprofiles";
+import AddDetails from "./scenes/profiles/addComorbidities";
+import HistoryPage1 from "./scenes//profiles/historyPage";
+import HistoryPage2 from "./scenes/profiles/historyPage2";
+import HistoryPage3 from "./scenes/profiles/historyPage3";
+import HistoryPage4 from "./scenes/profiles/historyPage4";
+import FinalPage from "./scenes/profiles/finalPage";
+import ManageTemplates from "./scenes/templates";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -32,6 +39,13 @@ function App() {
               <Route path="/signup" element={<SignUp/>}/>
               <Route path="/profiles" element={<PatientProfile/>}/>
               <Route path="/manageprofiles" element={<ManageProfile/>}/>
+              <Route path="templates" element={<ManageTemplates/>}/>
+              <Route path="/profiles/addComorbidities" element={<AddDetails/>}/>
+              <Route path="/profiles/historyPage" element={<HistoryPage1/>}/>
+              <Route path="/profiles/historyPage2" element={<HistoryPage2/>}/>
+              <Route path="/profiles/historyPage3" element={<HistoryPage3/>}/>
+              <Route path="/profiles/historyPage4" element={<HistoryPage4/>}/>
+              <Route path="/profiles/finalPage" element={<FinalPage/>}/>
             </Routes>
           </main>
         </div>
